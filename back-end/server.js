1ae4812cb715ffcc93fbd495807cfb872470440b
MunFahim <muntasirf362@gmail.com>
2025-09-19 12:14:38 -0400

    inital start

--- back-end/server.js
+++ back-end/server.js
@@ -1,24 +1,26 @@
-    // server.js
-    const express = require('express');
+    import { Socket, Server } from "socket.io";
+    import http from "http";
+    import express from 'express'
+
     const app = express();
     const port = 3000; 
 
+    const server = http.createServer(http);
     
     app.use(express.json());
 
-    
-    app.get('/', (req, res) => {
-        res.send('Hello from the Node.js backend!');
+    const io = new Server(server, {
+        cors: {
+            origin: "*"
+        }
     });
 
-    
-    app.post('/api/data', (req, res) => {
-        const receivedData = req.body;
-        console.log('Received data:', receivedData);
+    /*
+    server.get('/api/live-data', (req, res) => {
         res.json({ message: 'Data received successfully!', data: receivedData });
     });
+    */
 
-    
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(`Server listening on port ${port}`);
     });
